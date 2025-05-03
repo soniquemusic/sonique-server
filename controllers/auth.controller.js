3cd83fddb1ca7f4272a954fb5e7b8622c49c51a8
soniquemusic <soniquemusic07@gmail.com>
2025-05-03 15:42:36 +0530

    Token Time Change

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -54,7 +54,7 @@ exports.loginUser = async (req, res) => {
             return res.status(401).json({ error: 'Incorrect password.' });
         }
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '5m' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
         return res.status(200).json({
             token, user: { id: user._id, email: user.email, name: user.name, },
         });
